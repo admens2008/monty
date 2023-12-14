9914537b54e81c2757e7c892a47d26f3e3184ff9
admens2008 <adjeimensah@gmail.com>
2023-12-14 03:26:12 -0800

    swap_handler.c

--- swap_handler.c
+++ swap_handler.c
@@ -1,8 +1,9 @@
 #include "monty.h"
+
 /**
- * swap_handler - ==========
- * @head: ==========
- * @line_number: =========
+ * swap_handler - ====
+ * @head: ===
+ * @line_number: ===
  */
 void swap_handler(stack1_t **head, unsigned int line_number)
 {
@@ -16,19 +17,15 @@ void swap_handler(stack1_t **head, unsigned int line_number)
 		exit(EXIT_FAILURE);
 		return;
 	}
-
 	first = *head;
 	second = (*head)->next;
-
 	first->next = second->next;
 	if (second->next != NULL)
 	{
 		second->next->prev = first;
 	}
-
 	second->prev = NULL;
 	second->next = first;
 	first->prev = second;
-
 	info.head2 = second;
 }
